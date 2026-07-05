44ceaad93c7cfca52144ddea6d27b58bc6a0b1c2
Alex <81170092+magpie-engineering@users.noreply.github.com>
2026-07-05 06:40:46 +0100

    parallelise evaluation

--- mandelbrot/mandelbrot/mandelbrot.go
+++ mandelbrot/mandelbrot/mandelbrot.go
@@ -1,16 +1,34 @@
 package mandelbrot
 
 import (
+	"runtime"
+
 	"github.com/lucasb-eyer/go-colorful"
 )
 
 func CalcMandelbrot(screen []byte, screenHeight int, screenWidth int, minX float64, maxX float64, minI float64, maxI float64, colourScale []colorful.Color) {
+	n_goroutines := runtime.NumCPU()
+	rows_per_routine := screenHeight / n_goroutines
+
+	channels := make([]chan int, n_goroutines)
+
+	for routine := range n_goroutines {
+		channels[routine] = make(chan int)
+		go processRows(channels[routine], rows_per_routine*routine, rows_per_routine*(routine+1), screen, screenHeight, screenWidth, minX, maxX, minI, maxI, colourScale)
+	}
+
+	for _, ch := range channels {
+		<-ch
+	}
+
+}
+
+func processRows(c chan int, startRow int, endRow int, screen []byte, screenHeight int, screenWidth int, minX float64, maxX float64, minI float64, maxI float64, colourScale []colorful.Color) {
 	iRange := maxI - minI
 	xRange := maxX - minX
-
 	maxIter := len(colourScale) - 1
 
-	for screenI := 0; screenI < screenHeight; screenI++ {
+	for screenI := startRow; screenI < endRow && screenI < screenHeight; screenI++ {
 		i := (float64(screenI)/float64(screenHeight))*iRange + minI
 		for screenX := 0; screenX < screenWidth; screenX++ {
 			x := (float64(screenX)/float64(screenWidth))*xRange + minX
@@ -26,6 +44,7 @@ func CalcMandelbrot(screen []byte, screenHeight int, screenWidth int, minX float
 
 		}
 	}
+	close(c)
 
 }
 
